e15bcec0fdd9667e988513f4b1cc5a6533ea7aa6
Simon Willison <swillison@gmail.com>
2021-02-22 21:18:23 -0800

    More plugins

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ datasette publish cloudrun vaccinateca.db \
     --service vaccinateca \
     --branch 726f781c50e88f557437f6490b8479c3d6fabfc2 \
     --install datasette-cluster-map \
-    --install datasette-vega
+    --install datasette-vega \
+    --install datasette-graphql \
+    --install datasette-jellyfish
