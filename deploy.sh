ca26af5177b509b5dc4dc11f2fa96072f9f8e0f1
Simon Willison <swillison@gmail.com>
2021-02-22 16:00:38 -0800

    Removed authentication

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,5 @@ datasette publish cloudrun vaccinateca.db \
     --metadata metadata.yml \
     --template-dir templates \
     --service vaccinateca \
-    --install datasette-auth-github \
     --install datasette-cluster-map \
-    --install datasette-vega \
-    --plugin-secret datasette-auth-github client_secret $GITHUB_CLIENT_SECRET
+    --install datasette-vega
