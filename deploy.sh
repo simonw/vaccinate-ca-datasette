e58055905cb60669837751ba2a60cc80295f00b7
Simon Willison <swillison@gmail.com>
2021-02-22 16:23:36 -0800

    Deploy branch with bug fix
    Refs https://github.com/simonw/datasette/issues/1239

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ datasette publish cloudrun vaccinateca.db \
     --metadata metadata.yml \
     --template-dir templates \
     --service vaccinateca \
+    --branch 726f781c50e88f557437f6490b8479c3d6fabfc2 \
     --install datasette-cluster-map \
     --install datasette-vega
