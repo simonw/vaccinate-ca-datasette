51765490f81573a51c437f84a84badf728159ae8
Simon Willison <swillison@gmail.com>
2021-03-02 01:07:02 -0800

    Pipe through flatten-single-item-arrays

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #/bin/sh
 curl -s 'https://api.vaccinateca.com/v1/counties.json' | \
+    flatten-single-item-arrays - | \
     jq .content | sqlite-utils insert vaccinateca.db counties - --pk id
 curl -s 'https://api.vaccinateca.com/v1/locations.json' | \
+    flatten-single-item-arrays - | \
     jq .content | sqlite-utils insert vaccinateca.db locations - --pk id
 curl -s 'https://api.vaccinateca.com/v1/providers.json' | \
+    flatten-single-item-arrays - | \
     jq .content | sqlite-utils insert vaccinateca.db providers - --pk id
