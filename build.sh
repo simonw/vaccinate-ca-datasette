554fff029ad77c77fc09f202640642f2fb97a80b
Simon Willison <swillison@gmail.com>
2021-03-02 01:10:00 -0800

    Switched order of pipes

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #/bin/sh
 curl -s 'https://api.vaccinateca.com/v1/counties.json' | \
+    jq .content | \
     flatten-single-item-arrays - | \
-    jq .content | sqlite-utils insert vaccinateca.db counties - --pk id
+    sqlite-utils insert vaccinateca.db counties - --pk id
 curl -s 'https://api.vaccinateca.com/v1/locations.json' | \
+    jq .content | \
     flatten-single-item-arrays - | \
-    jq .content | sqlite-utils insert vaccinateca.db locations - --pk id
+    sqlite-utils insert vaccinateca.db locations - --pk id
 curl -s 'https://api.vaccinateca.com/v1/providers.json' | \
+    jq .content | \
     flatten-single-item-arrays - | \
-    jq .content | sqlite-utils insert vaccinateca.db providers - --pk id
+    sqlite-utils insert vaccinateca.db providers - --pk id
